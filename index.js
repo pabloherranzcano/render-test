1167a0cb7aee3171e3492028d889365453d9771a
Pablo <pablo.hc9@gmail.com>
2026-09-18 17:13:18 +0200

    static build

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 
 morgan.token('body', (req, res) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : '';
